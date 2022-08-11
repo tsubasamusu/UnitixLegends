47ec79e11dd78ae0fac584250ff7db291abe090a
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-11 20:25:52 +0900

    アイテムの切り替え処理を追加。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,10 @@ using System;//enumを使用
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
+	[SerializeField]
 	private float gravity;//重力
 
-    [SerializeField]
+	[SerializeField]
 	private float previousSpeed;//前進速度
 
 	[SerializeField]
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	private float speedX;//左右移動速度
 
-    [SerializeField]
+	[SerializeField]
 	private float jumpHeight;//ジャンプの高さ
 
 	[SerializeField]
@@ -36,16 +36,23 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	private CharacterController controller;//CharacterController
 
-    [SerializeField]
+	[SerializeField]
 	private Animator anim;//Animator
 
 	private Vector3 moveDirection = Vector3.zero;//進行方向ベクトル
+
+	private int useItemNo=1;//使用しているアイテムの番号
+
+	private int UseItemNo//useItemNo変数用のプロパティ
+    {
+		get { return useItemNo; }//取得処理のみ可能に
+    }
 
 	/// <summary>
 	/// Playerの状態
 	/// </summary>
 	private enum PlayerCondition
-    {
+	{
 		Idle,//何もしていない
 		MoveBack,//後進している
 		MovePrevious,//前進している
@@ -69,20 +76,20 @@ public class PlayerController : MonoBehaviour
 	/// </summary>
 	/// <param name="playerCondition"></param>
 	private void PlayAnimation(PlayerCondition playerCondition)
-    {
+	{
 		//アニメーション名
-		string animationName=null;
+		string animationName = null;
 
 		//Playerの状態に応じてアニメーション名を設定
-		switch(playerCondition)
-        {
-			case PlayerCondition.Idle: 
+		switch (playerCondition)
+		{
+			case PlayerCondition.Idle:
 				animationName = "Idle";
 				break;
 
-			case PlayerCondition.MoveBack: 
+			case PlayerCondition.MoveBack:
 				animationName = "MoveBack";
-				break ;
+				break;
 
 			case PlayerCondition.MovePrevious:
 				animationName = "MovePrevious";
@@ -111,7 +118,7 @@ public class PlayerController : MonoBehaviour
 			//指定したアニメーション名のアニメーションを再生
 			anim.Play(animationName);
 		}
-    }
+	}
 
 	/// <summary>
 	/// Playerの移動を制御
@@ -130,7 +137,7 @@ public class PlayerController : MonoBehaviour
 			//Playerの状態を返す
 			return PlayerCondition.Jumping;
 		}
-		
+
 		//Wを押されている間
 		if (Input.GetAxis("Vertical") > 0.0f)
 		{
@@ -212,43 +219,93 @@ public class PlayerController : MonoBehaviour
 	/// アイテム関連の処理
 	/// </summary>
 	private void ControlItem()
-    {
-		// 何かキーが押されたら
-		if (Input.anyKeyDown)
+	{
+		//何かキーが押されていたら
+		if (CheckKey() != KeyCode.None)//無駄な処理を回避
 		{
-			//1つずつキーの情報を取り出す
-			foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
-			{
-				//foreach文で取得したキーと、押されたキーが同じなら
-				if (Input.GetKeyDown(code))
-				{ 
+			//アイテムの切り替えを行う
+			ChangeItem(CheckKey());
 
-				}
+			//アイテム破棄キーが押されたら
+			if (Input.GetKeyDown(discardKey))
+			{
+				//TODO:アイテムを破棄する処理
+			}
+			//左クリックされている間
+			else if (Input.GetKey(KeyCode.Mouse0))
+			{
+				//TODO:アイテムの使用処理
+			}
+			//右クリックされている間
+			else if (Input.GetKey(KeyCode.Mouse1))
+			{
+				//アイテムを構える処理
 			}
 		}
-
-		//アイテム破棄キーが押されたら
-		if (Input.GetKeyDown(discardKey))
-        {
-			//TODO:アイテムを破棄する処理
-        }
-		//左クリックされている間
-		else if(Input.GetKey(KeyCode.Mouse0))
-        {
-			//TODO:アイテムの使用処理
-        }
-		//右クリックされている間
-		else if(Input.GetKey(KeyCode.Mouse1))
-        {
-			//アイテムを構える処理
-        }
 
 		//TODO:アイテムに近づいたら
 
 		//アイテム取得キーが押されたら
-		if(Input.GetKeyDown(getItemKey))
-        {
+		if (Input.GetKeyDown(getItemKey))
+		{
 			//TODO:アイテムの取得処理
-        }
+		}
+	}
+
+	/// <summary>
+	/// 押されたキーの情報を返す
+	/// </summary>
+	private KeyCode CheckKey()
+	{
+		//何もキーを押されていないなら
+		if (!Input.anyKeyDown)//無駄な処理を回避
+		{
+			//キーの情報を返す
+			return KeyCode.None;
+		}
+
+		//1つずつキーの情報を取り出す
+		foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+		{
+			//foreach文で取得したキーと、押されたキーが同じなら
+			if (Input.GetKeyDown(code))
+			{
+				//キーの情報を返す
+				return code;
+			}
+		}
+
+		//キーの情報を返す
+		return KeyCode.None;
+	}
+
+	/// <summary>
+	/// 使用アイテムの切り替えを行う
+	/// </summary>
+	private void ChangeItem(KeyCode code)
+    {
+		//押されたキーに応じて使用しているアイテムの番号を設定
+        switch (code)
+        {
+			case KeyCode.Alpha1:
+				useItemNo = 1;
+				break;
+
+			case KeyCode.Alpha2:
+				useItemNo = 2;
+				break;
+
+			case KeyCode.Alpha3:
+				useItemNo= 3;
+				break;
+
+			case KeyCode.Alpha4:
+				useItemNo = 4;
+				break;
+
+			case KeyCode.Alpha5:
+				useItemNo = 5;
+				break;
+		}
     }
 }
