021c18c52028014b42d7aef7e2362d9bcc97ad6c
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-11 19:52:48 +0900

    「Animator」の設定を行い、「PlayerController.cs」も修正した。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;//DOTweenを使用
+using System;//enumを使用
 
 public class PlayerController : MonoBehaviour
 {
@@ -21,10 +22,16 @@ public class PlayerController : MonoBehaviour
 	private float jumpHeight;//ジャンプの高さ
 
 	[SerializeField]
-	private KeyCode jumpKey;//ジャンプボタン
+	private KeyCode jumpKey;//ジャンプキー
 
 	[SerializeField]
-	private KeyCode stoopKey;//かがむボタン
+	private KeyCode stoopKey;//かがむキー
+
+	[SerializeField]
+	private KeyCode getItemKey;//アイテム取得キー
+
+	[SerializeField]
+	private KeyCode discardKey;//アイテム破棄キー
 
 	[SerializeField]
 	private CharacterController controller;//CharacterController
@@ -37,86 +44,148 @@ public class PlayerController : MonoBehaviour
 	/// <summary>
 	/// Playerの状態
 	/// </summary>
-	public enum PlayerCondition
+	private enum PlayerCondition
     {
 		Idle,//何もしていない
 		MoveBack,//後進している
 		MovePrevious,//前進している
-		MoveX,//左右移動している
+		MoveRight,//右移動している
+		MoveLeft,//左移動している
 		Jumping,//ジャンプしている
 		Stooping//かがんでいる
 	}
 
 	void Update()
 	{
-		//Playerの移動を制御
-		MovePlayer();
+		//アニメーションの再生と、Playerの動きの制御を行う
+		PlayAnimation(MovePlayer());
+
+		//アイテムを制御
+		ControlItem();
 	}
+
+	/// <summary>
+	/// 受け取ったPlayerの状態を元に、アニメーションの再生を行う
+	/// </summary>
+	/// <param name="playerCondition"></param>
+	private void PlayAnimation(PlayerCondition playerCondition)
+    {
+		//アニメーション名
+		string animationName=null;
+
+		//Playerの状態に応じてアニメーション名を設定
+		switch(playerCondition)
+        {
+			case PlayerCondition.Idle: 
+				animationName = "Idle";
+				break;
+
+			case PlayerCondition.MoveBack: 
+				animationName = "MoveBack";
+				break ;
+
+			case PlayerCondition.MovePrevious:
+				animationName = "MovePrevious";
+				break;
+
+			case PlayerCondition.MoveRight:
+				animationName = "MoveRight";
+				break;
+
+			case PlayerCondition.MoveLeft:
+				animationName = "MoveLeft";
+				break;
+
+			case PlayerCondition.Jumping:
+				animationName = "Jumping";
+				break;
+
+			case PlayerCondition.Stooping:
+				animationName = "Stooping";
+				break;
+		}
+
+		//nullエラー回避
+		if (animationName != null)
+		{
+			//指定したアニメーション名のアニメーションを再生
+			anim.Play(animationName);
+		}
+    }
 
 	/// <summary>
 	/// Playerの移動を制御
 	/// </summary>
 	private PlayerCondition MovePlayer()
-    {
-		//重力を発生
+	{
+		//重力を生成
 		moveDirection.y -= gravity * Time.deltaTime;
 
 		//ローカル空間からワールド空間へdirectionを変換し、その向きと大きさに移動
 		controller.Move(transform.TransformDirection(moveDirection) * Time.deltaTime);
 
-		//Playerが接地しているなら
-		if (CheckGrounded())
+		//Playerが接地していないなら
+		if (!CheckGrounded())
 		{
-			//Wを押されている間
-			if (Input.GetAxis("Vertical") > 0.0f)
-			{
-				//進行方向ベクトルを設定
-				moveDirection.z = Input.GetAxis("Vertical") * previousSpeed;
-
-				//Playerの状態を返す
-				return PlayerCondition.MovePrevious;
-			}
-			//Sを押されている間
-			else if (Input.GetAxis("Vertical") < 0.0f)
-			{
-				//進行方向ベクトルを設定
-				moveDirection.z = Input.GetAxis("Vertical") * backSpeed;
-
-				//Playerの状態を返す
-				return PlayerCondition.MoveBack;
-			}
-
-			//AかDを押されている間
-			if (Input.GetAxis("Horizontal") != 0.0f)
-			{
-				//進行方向ベクトルを設定
-				moveDirection.x = Input.GetAxis("Horizontal") * speedX;
-
-				//Playerの状態を返す
-				return PlayerCondition.MoveX;
-			}
-
-			//ジャンプボタンが押されたら
-			if (Input.GetKeyDown(jumpKey))
-			{
-				//jumpHeightの高さへ0.5秒かけて移動して、元の位置に戻る
-				transform.DOMove(new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z), 0.5f).SetLoops(2, LoopType.Yoyo);
-
-				//Playerの状態を返す
-				return PlayerCondition.Jumping;
-			}
-
-			//かがむボタンが押されている間
-			if (Input.GetKey(stoopKey))
-			{
-				//Playerの状態を返す
-				return PlayerCondition.Stooping;
-
-				//TODO:かがむ処理
-			}
+			//Playerの状態を返す
+			return PlayerCondition.Jumping;
+		}
+		
+		//Wを押されている間
+		if (Input.GetAxis("Vertical") > 0.0f)
+		{
+			//進行方向ベクトルを設定
+			moveDirection.z = Input.GetAxis("Vertical") * previousSpeed;
 
 			//Playerの状態を返す
-			return PlayerCondition.Idle;
+			return PlayerCondition.MovePrevious;
+		}
+		//Sを押されている間
+		else if (Input.GetAxis("Vertical") < 0.0f)
+		{
+			//進行方向ベクトルを設定
+			moveDirection.z = Input.GetAxis("Vertical") * backSpeed;
+
+			//Playerの状態を返す
+			return PlayerCondition.MoveBack;
+		}
+
+		//Dを押されている間
+		if (Input.GetAxis("Horizontal") > 0.0f)
+		{
+			//進行方向ベクトルを設定
+			moveDirection.x = Input.GetAxis("Horizontal") * speedX;
+
+			//Playerの状態を返す
+			return PlayerCondition.MoveRight;
+		}
+		//Aを押されている間
+		else if (Input.GetAxis("Horizontal") < 0.0f)
+		{
+			//進行方向ベクトルを設定
+			moveDirection.x = Input.GetAxis("Horizontal") * speedX;
+
+			//Playerの状態を返す
+			return PlayerCondition.MoveLeft;
+		}
+
+		//ジャンプキーが押されたら
+		if (Input.GetKeyDown(jumpKey))
+		{
+			//jumpHeightの高さへ0.5秒かけて移動して、元の位置に戻る
+			transform.DOMove(new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z), 0.5f).SetLoops(2, LoopType.Yoyo);
+
+			//Playerの状態を返す
+			return PlayerCondition.Jumping;
+		}
+
+		//かがむキーが押されている間
+		if (Input.GetKey(stoopKey))
+		{
+			//TODO:かがむ処理
+
+			//Playerの状態を返す
+			return PlayerCondition.Stooping;
 		}
 
 		//Playerの状態を返す
@@ -138,4 +207,48 @@ public class PlayerController : MonoBehaviour
 		//rayのヒット判定（bool型）を返す
 		return Physics.Raycast(ray, tolerance);
 	}
+
+	/// <summary>
+	/// アイテム関連の処理
+	/// </summary>
+	private void ControlItem()
+    {
+		// 何かキーが押されたら
+		if (Input.anyKeyDown)
+		{
+			//1つずつキーの情報を取り出す
+			foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+			{
+				//foreach文で取得したキーと、押されたキーが同じなら
+				if (Input.GetKeyDown(code))
+				{ 
+
+				}
+			}
+		}
+
+		//アイテム破棄キーが押されたら
+		if (Input.GetKeyDown(discardKey))
+        {
+			//TODO:アイテムを破棄する処理
+        }
+		//左クリックされている間
+		else if(Input.GetKey(KeyCode.Mouse0))
+        {
+			//TODO:アイテムの使用処理
+        }
+		//右クリックされている間
+		else if(Input.GetKey(KeyCode.Mouse1))
+        {
+			//アイテムを構える処理
+        }
+
+		//TODO:アイテムに近づいたら
+
+		//アイテム取得キーが押されたら
+		if(Input.GetKeyDown(getItemKey))
+        {
+			//TODO:アイテムの取得処理
+        }
+    }
 }
