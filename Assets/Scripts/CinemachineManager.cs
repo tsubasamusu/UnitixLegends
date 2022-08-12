cfb6b25ddb483bd1637530fb4c36a9e518d499ee
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-12 09:42:24 +0900

    「CinemachineManager.cs」が完成。

--- Assets/Scripts/CinemachineManager.cs
+++ Assets/Scripts/CinemachineManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CinemachineManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private CinemachineFreeLook airplaneCamera;//飛行機視点カメラ
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 飛行機視点カメラの優先順位を設定
+    /// </summary>
+    /// <param name="airplaneCameraPriority"></param>
+    public void SetAirplaneCameraPriority(int airplaneCameraPriority)
     {
-        
+        //引数を元に、飛行機視点カメラの優先順位を設定
+        airplaneCamera.Priority= airplaneCameraPriority;
     }
 }
