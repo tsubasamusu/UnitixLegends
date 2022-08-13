65d2884afa97ac8918313ea0946ada5bb10cdb78
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-13 14:34:51 +0900

    ゲームスタート演出を追加。

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//UIを使用
+using DG.Tweening;//DOTweenを使用
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Image eventHorizon;//視界
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private Image logo;//ロゴ
+
+    [SerializeField]
+    private Sprite gameStart;//ゲームスタートロゴ
+
+    [SerializeField]
+    private Sprite gameClear;//ゲームクリアロゴ
+
+    /// <summary>
+    /// ゲームスタート演出を行う
+    /// </summary>
+    public IEnumerator SetGameStart()
     {
-        
+        //視界を白色に設定
+        eventHorizon.color = new Color(255.0f, 255.0f, 255.0f);
+
+        //ロゴをゲームスタートに設定
+        logo.sprite = gameStart;
+
+        //2.0秒かけてロゴを表示
+        logo.DOFade(1.0f,12.0f);
+
+        //ロゴの表示が終わるまで待つ
+        yield return new WaitForSeconds(2.0f);
+
+        //視界を1.0秒掛けて透明にする
+        eventHorizon.DOFade(0.0f, 1.0f);
+
+        //ロゴを1.0秒かけて消す
+        logo.DOFade(0.0f, 1.0f);
+
+        //視界とロゴの演出が終わるまで待つ
+        yield return new WaitForSeconds(1.0f);
+
+        //GameManagerからゲーム開始状態に切り替える
     }
 }
