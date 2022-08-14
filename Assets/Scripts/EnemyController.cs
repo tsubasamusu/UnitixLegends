c32bb0d46b716b237bc6fd554f3a226bfdabbf3a
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-14 16:32:45 +0900

    NavMeshを追加。

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private NavMeshAgent agent;//NavMeshAgent
 }
