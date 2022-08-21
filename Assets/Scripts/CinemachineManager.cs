8f630d4ea18c238278b6a39f9bc63794ac9e0d48
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-21 13:20:07 +0900

    Playerのキャラクターが視界を遮る問題を修正。

--- Assets/Scripts/CinemachineManager.cs
+++ Assets/Scripts/CinemachineManager.cs
@@ -14,13 +14,61 @@ public class CinemachineManager : MonoBehaviour
     [SerializeField]
     private Transform playerTran;//Playerの位置
 
+    [SerializeField]
+    private Transform mainCameraTran;//メインカメラの位置
+
+    [SerializeField]
+    private Transform playerCharacterTran;//Playerのキャラクターの位置
+
+    private float angle;//Playerのキャラクターと視点が被る角度
+
+    /// <summary>
+    /// ゲーム開始直後に呼び出される
+    /// </summary>
+    private void Start()
+    {
+        //Playerのキャラクターと視点が被る角度を取得
+        angle = playerCharacterTran.eulerAngles.y + 90f;
+    }
+
     /// <summary>
     /// 毎フレーム呼び出される
     /// </summary>
     private void Update()
     {
         //ミニマップの背景の位置を常にPlayerの位置に合わせる
-        miniMapBackgroundTran.position = new Vector3(playerTran.position.x,miniMapBackgroundTran.position.y,playerTran.position.z);
+        miniMapBackgroundTran.position = new Vector3(playerTran.position.x, miniMapBackgroundTran.position.y, playerTran.position.z);
+
+        //Playerのキャラクターが射撃の邪魔にならないようにする
+        SetPlayerCharacter();
+    }
+
+    /// <summary>
+    /// カメラの角度に応じてPlayerのキャラクターの有効化、無効化を切り替える
+    /// </summary>
+    private void SetPlayerCharacter()
+    {
+        //カメラの角度が一定範囲内なら
+        if (mainCameraTran.eulerAngles.y >= angle - 20f && mainCameraTran.eulerAngles.y <= angle + 20f)
+        {
+            //Playerのキャラクターを無効化
+            playerCharacterTran.gameObject.SetActive(false);
+
+            //以降の処理を行わない
+            return;
+        }
+        //カメラの角度が一定範囲内なら
+        else if (mainCameraTran.eulerAngles.y >= (angle + 180f) - 20f && mainCameraTran.eulerAngles.y <= (angle + 180f) + 20f)
+        {
+            //Playerのキャラクターを無効化
+            playerCharacterTran.gameObject.SetActive(false);
+
+            //以降の処理を行わない
+            return;
+        }
+
+        //Playerのキャラクターを有効化
+        playerCharacterTran.gameObject.SetActive(true);
     }
 
     /// <summary>
