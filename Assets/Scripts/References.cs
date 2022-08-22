b4f0375b8a93b50aedb6cd45cfb14a201a85cace
mitsuruyamaura <komayou326@gmail.com>
2022-08-22 10:13:26 +0900

    リファクタリング用ブランチ１。

---
+++ Assets/Scripts/References.cs
@@ -0,0 +1,10 @@
+public class References
+{
+    // GameData を GameData と ItemManager に分割。どちらもシングルトンにする
+
+    // BulletManager ではなくて、WeaponManager にして、弾も武器も管理する
+
+    // Bullet と Weapon にクラスをアタッチする。継承させる
+
+
+}
